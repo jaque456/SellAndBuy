5ca6afbbaf8d5a1d0ded3e96899d0a7da1602f57
Jaqueline <padronjaque456@gmail.com>
2021-07-27 18:17:43 -0500

    Cambios
    Creacion de las variables y inserciones

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -7,9 +7,21 @@ router.get('/publicarProd', isLoggedIn, (req, res) => {
     req.flash('success', 'Producto publicado correctamente');
     res.render('productos/publicarProd');
 });
+router.post ('publicarProd',async), (req, res) =>{
+    const { CantidadStock, DescripProducto, PrecioProducto} = req.body;
+    const Produc = {
+        CantidadStock,
+        DescripProducto,
+        PrecioProducto
+    };
+     await pool.query('INSERT INTO Producto set ?, {Produc} ');
+    res.send('recivido');
+}
 
-router.get('/consultProdu', isLoggedIn, (req, res) => {
-    res.render('productos/consultProdu');
+router.get('/consultProdu', async, isLoggedIn, (req, res) => {
+    const Consulta = await pool.query('SELECT * FROM Producto' );
+    res.render('productos/consultProdu',{Consulta});
+    
 });
 
 module.exports = router;
